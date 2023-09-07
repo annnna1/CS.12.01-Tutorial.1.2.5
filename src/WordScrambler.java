ea8ad7b77521414421f61e19b10fb7c634823b79
annnna1 <123133994+annnna1@users.noreply.github.com>
2023-09-07 10:16:34 +0800

    Create WordScrambler.java

---
+++ src/WordScrambler.java
@@ -0,0 +1,50 @@
+public class WordScrambler
+{
+    private String[] scrambledWords;
+
+    public WordScrambler(String[] wordArr) {
+
+        scrambledWords = mixedWords(wordArr);
+
+    }
+
+    private String recombine(String word1, String word2) {
+
+        String s = "";
+        s+= word1.substring(0, word1.length()/2);
+        s+= word2.substring(word2.length()/2, word2.length());
+        return s;
+    }
+
+    private String[] mixedWords(String[] words)
+    {
+        if(words.length%2 == 1)
+        {
+            throw new IllegalArgumentException("Word Array must contain an even number of elements!");
+        }
+        String[] w = new String[words.length];
+        for(int i = 0 ; i < words.length; i ++)
+        {
+            if(words[i].indexOf(32)!=-1)
+            {
+                throw new IllegalArgumentException("Word Array must contain single words only! Attack on Titan contains more than one word!");
+            }
+            String s = "";
+            if(i%2==0)
+            {
+                w[i] = recombine(words[i] , words[i + 1]);
+            }
+            else
+            {
+                w[i] = recombine(words[i] , words[i - 1]);
+            }
+        }
+        return w;
+    }
+
+    public String[] getScrambledWords()
+    {
+        return scrambledWords;
+    }
+
+}
